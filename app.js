26983922fc63e16a7cafc540026c6e0c5ac8053f
Qutadah95 <>
2021-06-16 13:10:13 +0000

    0

---
+++ app.js
@@ -0,0 +1,35 @@
+
+alert("welcome to The online libarary  ")
+
+var section = prompt("What is your favorite section From these section: Book , Manga , Scientific Books , Literary Books?")
+
+
+
+
+
+
+if(section == "Book" ){
+ alert("we have your favorite books  ")
+
+}
+ else if(section == "Manga")
+{
+ alert("we have your favorite books  ")
+
+}
+
+else if(section == "Scientific Books")
+{
+ alert("we have your favorite books  ")
+ 
+}
+else if(section == "Literary Books")
+{
+ alert("we have your favorite books  ")
+ 
+}
+else{
+  alert("sorry we don't have this Type of books")
+}
+
+
