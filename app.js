09ea6f985a5cca13be463fba8779265c5a2c95d3
Qutadah95 <>
2021-06-17 13:36:22 +0000

    final one

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 
 alert("welcome to The online libarary  ")
 
-var section = prompt("What is your favorite section From these section: Book , Manga , Scientific Books , Literary Books?")
+var solve = prompt("please do the puzzle to you can access our website: 1 + 1 = ")
+if(solve == 2){
+  alert("thanks for solve the puzzle enjoy in the website")
+}
+else{
+  alert("Do it again")
+  
+}
+while (solve !=2)
+{solve = prompt("please do the puzzle to you can access our website: 1 + 1 = ")
 
+}
+ 
+var num = prompt("how many time you want to show a picture of our most viewed books  ")
+var i;
+for (i = 0; i < num; i++) {
+document.write("<img src='https://orion-uploads.openroadmedia.com/sm_f7e651-tolkien-lordoftherings.jpg' alt='the lord of the ring'>");
+}
 
-
-
+// var section = prompt("What is your favorite section From these section: Book , Manga , Scientific Books , Literary Books?")
 
 
 if(section == "Book" ){
